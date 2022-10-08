8a70d67a888e2a67a9af1f103dd68ebb6edfdfa2
moipo <moipochtoviyyashik@inbox.ru>
2022-10-08 15:33:50 +0300

    ready for deployment: MEDIA_ROOT + collecstatic

--- tester/settings.py
+++ tester/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9f942492a4x+dv5)n(^jho!jta+agwu8@_0qksg+6ro8_op*8t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
--- univ_app/urls.py
+++ univ_app/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('',General.homepage, name = "homepage"),
     path('create_test/',General.create_test, name = 'create_test'),
     path('create_test/<int:testid>/create_questions/',General.create_questions, name = "create_questions"),
